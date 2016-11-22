bea33a4b11d307aefda371bcbdd95ba740af9566
Steve Zelaznik <zelaznik@yahoo.com>
2016-11-22 17:57:16 -0600

    election web scraping

---
+++ tutorial/spiders/house_spider.py
@@ -0,0 +1,158 @@
+import scrapy, json, os, re
+from collections import OrderedDict
+from contextlib import suppress
+
+class HouseSpider(scrapy.Spider):
+    name         = 'house'
+
+    __outputPath = '/Users/zMac/Desktop/tutorial/house_results.json'
+    __baseUrl    = 'http://www.politico.com/2016-election/results/map/house'
+    __states     = ('alabama','alaska','arizona','arkansas','california',
+                   'colorado','connecticut','delaware','florida','georgia',
+                   'hawaii','idaho','illinois','indiana','iowa','kansas',
+                   'kentucky','louisiana','maine','maryland','massachusetts',
+                   'michigan','minnesota','mississippi','missouri','montana',
+                   'nebraska','nevada','new-hampshire','new-jersey','new-mexico',
+                   'new-york','north-carolina','north-dakota','ohio','oklahoma',
+                   'oregon','pennsylvania','rhode-island','south-carolina',
+                   'south-dakota','tennessee','texas','utah','vermont','virginia',
+                   'washington','west-virginia','wisconsin','wyoming')
+
+    def start_requests(self):
+        self.__clearPickledFile()
+        for url in self.__urls():
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        state_name       = response.url.rstrip('/').split('/')[-1]
+        election_results = response.css('section.content-group.election-results')
+        districts        = election_results.css('article.results-group')
+
+        for i, district in enumerate(districts):
+            district_number = i+1
+            pure_results    = self.__parseDictrict(district)
+            new_results     = self.__concatDistrictData(state_name, district_number, pure_results)
+            self.__appendPickledFile(new_results)
+
+    @staticmethod
+    def __humanize(state_name):
+        words = state_name.split('-')
+        capitalized = ['%s%s' % (word[0].upper(), word[1:].lower()) for word in words]
+        return ' '.join(capitalized)
+
+    def __concatDistrictData(self, state_name, district, pure_results):
+        new_results               = OrderedDict()
+        new_results['state_name'] = self.__humanize(state_name)
+        new_results['district']   = district
+
+        for key in pure_results:
+            new_results[key] = pure_results[key]
+
+        return new_results
+
+    def __readPickledFile(self):
+        with open(self.__outputPath, 'r') as f:
+            return json.loads(f.read(), object_pairs_hook=OrderedDict)
+
+    def __overwritePickledFile(self, data):
+        with open(self.__outputPath, 'w') as f:
+            f.write(json.dumps(data, indent=2))
+
+    def __clearPickledFile(self):
+        blank_data = []
+        self.__overwritePickledFile(blank_data)
+
+    def __appendPickledFile(self, district_results):
+        prev_results = self.__readPickledFile()
+        all_results  = prev_results + [district_results]
+        self.__overwritePickledFile(all_results)
+
+    def __urls(self):
+        for state in self.__states:
+            yield '%s/%s/' % (self.__baseUrl, state)
+
+    def __parseDictrict(self, district):
+        data   = OrderedDict()
+        handle = self.__handler()
+
+        democrat   = handle(self.__getDemocrats, district)
+        republican = handle(self.__getRepublicans, district)
+        other      = handle(self.__getIndependents, district)
+
+        data['contested']          = not not (democrat and republican)
+
+        data['democrat_name']      = democrat   and democrat.get('name')
+        data['republican_name']    = republican and republican.get('name')
+
+        data['democrat_votes']     = democrat   and democrat.get('votes')
+        data['republican_votes']   = republican and republican.get('votes')
+
+        data['democrat_percent']   = democrat   and democrat.get('percent')
+        data['republican_percent'] = republican and republican.get('percent')
+
+        data['democrat_winner']    = democrat.get('winner')   if democrat   else False
+        data['republican_winner']  = republican.get('winner') if republican else False
+
+        data['other_candidates']   = other
+
+        return data
+
+    def __getTitle(self, district):
+        pass
+        pass
+
+    def __getDemocrats(self, district):
+        with suppress(IndexError):
+            return self.__parseParty(district.css('tr.type-democrat')[-1])
+
+    def __getRepublicans(self, district):
+        with suppress(IndexError):
+            return self.__parseParty(district.css('tr.type-republican')[-1])
+
+    def __getIndependents(self, district):
+        independents = district.css('tr.type-independent')
+        return [self.__parseParty(i) for i in independents]
+
+    def __parseParty(self, party):
+        data                 = OrderedDict()
+        handle               = self.__handler()
+
+        data['winner']       = handle(self.__extractWinner,     party)
+        data['name']         = handle(self.__extractName,       party)
+        data['votes']        = handle(self.__extractVotes,      party)
+        data['percent']      = handle(self.__extractPercentage, party)
+
+        return data
+
+    def __extractWinner(self, party):
+        return not not party.css('span.token.token-winner').extract_first()
+
+    def __extractName(self, party):
+        matches = party.css('th.results-name > span.name-combo::text')
+        return matches.extract()[-1].strip()
+
+    def __extractVotes(self, party):
+        as_text = party.css('td.results-popular::text').extract_first()
+        if as_text != None:
+            return int(as_text.replace(',',''))
+
+    def __extractPercentage(self, party):
+        as_text = party.css('td.results-percentage span.number::text').extract_first()
+        if as_text != None:
+            return round(float(as_text.replace('%','')) * 0.01, 3)
+
+    def __handler(self):
+        errorOccurred = False
+        def handle(callback, *args):
+            try:
+                data = callback(*args)
+            except Exception as e:
+                data = {'error': '%s: %s' % (type(e).__name__, str(e))}
+                errorOccurred = True
+            return data
+
+        def error_raised():
+            return errorOccurred
+
+        handle.error_raised = error_raised
+        return handle
--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -1,21 +1,51 @@
 import scrapy
+import json
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
 
-    def start_requires(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1',
-            'http://quotes.toscrape.com/page/2',
+    def start_requests(self):
+        base_url = 'http://www.politico.com/2016-election/results/map/house'
+        states   = [
+           'alabama','alaska','arizona','arkansas','california',
+           'colorado','connecticut','delaware','florida','georgia',
+           'hawaii','idaho','illinois','indiana','iowa','kansas',
+           'kentucky','louisiana','maine','maryland','massachusetts',
+           'michigan','minnesota','mississippi','missouri','montana',
+           'nebraska','nevada','new-hampshire','new-jersey','new-mexico',
+           'new-york','north-carolina','north-dakota','ohio','oklahoma',
+           'oregon','pennsylvania','rhode-island','south-carolina',
+           'south-dakota','texas','utah','vermont','virginia',
+           'washington','west-virginia','wisconsin','wyoming'
         ]
-        for url in urls:
-            yield scrapy.Require(url=url, callback=self.parse)
+
+        for state in states:
+            url = '%s/%s/' % (base_url, state)
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page     = response.url.split('/')[-2]
-        filename = 'quotes-%s.html' % (page,)
+        state_name       = response.url.rstrip('/').split('/')[-1]
+        election_results = response.css('section.content-group.election-results')
+        districts        = election_results.css('article.results-group')
 
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        for i, district in enumerate(districts):
+            results = self.parse_district(district)
+            results.update({'state': state_name, 'district': i})
+            with open('election_results.json', 'a') as dst:
+                dst.write('%s,\n' % (json.dumps(results),))
 
-        self.log('Saved file %s' % (filename,))
+    def parse_district(self, district):
+        republican, *_ = district.css('tr.type-republican')
+        democrat, *_   = district.css('tr.type-democrat')
+
+        return {
+            'republican': self.parse_party(republican),
+            'democrat':   self.parse_party(democrat)
+        }
+
+    def parse_party(self, party):
+        return {
+            'votes':      party.css('td.results-popular::text').extract_first(),
+            'percentage': party.css('td.results-percentage span.number::text').extract_first(),
+            'name':       party.css('th.results-name > span.name-combo::text').extract()[-1].strip()            
+        }
